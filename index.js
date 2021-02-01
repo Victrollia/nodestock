8eef689822b4697aa072b10d4b540916fa9d6447
Victoria Cadogan <vicasmail@gmail.com>
2021-01-31 20:28:59 -0500

    created procfile for heroku

--- index.js
+++ index.js
@@ -6,17 +6,52 @@ const exphbs  = require('express-handlebars');
 const app = express()
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+const request = require('request');
+const bodyParser = require('body-parser');
+
+//use body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+//create call_api function
+function call_api(finishedAPI, ticker) {
+	request('https://cloud-sse.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_9d0069978cca4c4685b658b67dfbefb3', {json: true }, (err, res, body) => {
+	if (err) {return console.log(err);}
+	if (res.statusCode === 200){
+		//console.log(body);
+		finishedAPI(body);
+		};
+	});	
+};
+
 
 //set handlebars middleware
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 const otherstuff = "hello there, this is other stuff"
-//set handlebar routes
+
+//set handlebar index GET routes
 app.get('/', function (req, res) {
-    res.render('home', {
-    	stuff: otherstuff
-    });
+    call_api(function(doneAPI) {
+    	res.render('home', {
+    	stock: doneAPI
+    });	
+  }, "fb");   
+});
+
+//set handlebar index POST routes
+app.post('/', function (req, res) {
+    call_api(function(doneAPI) {
+    	//posted_stuff = req.body.stock_ticker;
+    	res.render('home', {
+    	stock: doneAPI,
+    });	
+  }, req.body.stock_ticker);    
+});
+
+//create about page route
+app.get('/about.html', function (req, res) {
+    res.render('about');
 });
 
 //set static folder
